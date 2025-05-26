c62f000d38cc05edad5c6ae1f994abd90c81e903
Rory James <rory.james2021@gmail.com>
2025-05-26 12:13:48 -0700

    patch data fetching to support base path

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,13 @@
 import type { LeetCodeProblem } from "./types";
 
 export async function getData(): Promise<LeetCodeProblem[]> {
-  const res = await fetch('/data.json');
+  let res = await fetch('/data.json');
   if (!res.ok) {
-    throw new Error('Failed to fetch problem data');
+    res = await fetch('/leet-tracker/data.json');
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch problem data');
+    }
   }
 
   const problems: LeetCodeProblem[] = await res.json();
