dce52a1e28317c60718bc252dc993c35e064b273
Rory James <rory.james2021@gmail.com>
2025-05-25 19:13:25 -0700

    integrate new data into table

--- src/data.ts
+++ src/data.ts
@@ -1,37 +1,12 @@
 import type { LeetCodeProblem } from "./types";
 
 export async function getData(): Promise<LeetCodeProblem[]> {
-  const res = await fetch('/ratings.txt');
+  const res = await fetch('/data.json');
   if (!res.ok) {
     throw new Error('Failed to fetch problem data');
   }
 
-  const file = await res.text();
-
-  const problems: LeetCodeProblem[] = [];
-
-  for (const line of file.split('\n')) {
-    const items = line.trim().split('\t');
-    
-    const rating = Number(items[0]);
-    const id = Number(items[1]);
-    const name = items[2];
-    const slug = items[4];
-
-    problems.push({
-      id,
-      name,
-      slug,
-      rating,
-      completed: false
-    })
-  }
-
-  problems.shift(); // Remove the header line
-  problems.pop(); // Remove the last empty line if it exists
-
-  console.log('Loaded problems:', problems.length);
-  console.log(problems);
+  const problems: LeetCodeProblem[] = await res.json();
 
   return problems;
 }
