7e844364f07edacee61aba147f66c1aa49376dd8
Rory James <rory.james2021@gmail.com>
2025-05-25 18:10:52 -0700

    cleanup app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css'
 import LeetTable from './components/LeetTable'
-import { getData } from './data'
 
 function App() {
 
-  const [problems, setProblems] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const data = await getData();
-        console.log('Fetched problems:', data.length);
-        setProblems(data);
-      } catch (error) {
-        console.error('Error fetching problem data:', error);
-      }
-    })();
-  }, []);
-
   return (
     <>
-      <LeetTable initialProblems={problems}/>
+      <LeetTable />
     </>
   )
 }
